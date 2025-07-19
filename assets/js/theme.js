139866ac450991ef84c6a49ac0af0ae6e04dc2a6
moth <failure@everyturn.net>
2025-07-18 22:59:58 -0400

    Publish initial website

---
+++ assets/js/theme.js
@@ -0,0 +1,58 @@
+// Theme changer code
+(function () {
+  const initializeTheme = () => {
+    let currentTheme = localStorage.getItem("theme");
+
+    if (!currentTheme) {
+      const prefersDark = window.matchMedia(
+        "(prefers-color-scheme: dark)"
+      ).matches;
+      currentTheme = prefersDark ? "dim" : "wireframe";
+    }
+
+    console.log("Initializing theme:", currentTheme);
+    document.documentElement.setAttribute("data-theme", currentTheme);
+    updateThemeToggleButton(currentTheme);
+  };
+
+  const toggleTheme = () => {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    const newTheme = currentTheme === "dim" ? "wireframe" : "dim";
+    //console.log("Toggling theme from", currentTheme, "to", newTheme);
+    document.documentElement.setAttribute("data-theme", newTheme);
+    localStorage.setItem("theme", newTheme);
+    updateThemeToggleButton(newTheme);
+  };
+
+  const toggleDropdownMenu = () => {
+    const dropdownMenu = document.getElementById("dropdownMenu");
+    const isExpanded = dropdownMenu.classList.toggle("hidden");
+    const dropdownButton = document.getElementById("dropdownButton");
+    dropdownButton.setAttribute("aria-expanded", !isExpanded);
+  };
+
+  const updateThemeToggleButton = (theme) => {
+    const themeToggleButton = document.getElementById("themeToggleButton");
+    const isDarkTheme = theme === "dim";
+    themeToggleButton.checked = isDarkTheme;
+    //console.log("Updated theme toggle button:", themeToggleButton.checked);
+  };
+
+  document.addEventListener("DOMContentLoaded", () => {
+    initializeTheme();
+
+    const themeToggleButton = document.getElementById("themeToggleButton");
+    if (themeToggleButton) {
+      themeToggleButton.addEventListener("change", toggleTheme);
+    } else {
+      //console.error("Theme toggle button not found on DOMContentLoaded");
+    }
+
+    const dropdownButton = document.getElementById("dropdownButton");
+    if (dropdownButton) {
+      dropdownButton.addEventListener("click", toggleDropdownMenu);
+    } else {
+      //console.error("Dropdown button not found on DOMContentLoaded");
+    }
+  });
+})();
